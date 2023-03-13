2e76af71466898c8519c026e854f8a6a8b0e8ad5
ElievertonG <104122097+Elieverton6@users.noreply.github.com>
2023-03-13 02:19:18 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import './App.css';
+import HelloWorld from './components/HelloWorld'
+
+function App() {
+
+  const name = "Elieverton";
+  const lastName = "Gomes";
+
+  const newName = name.toUpperCase();
+  const newLastName = lastName.toUpperCase();
+
+  function sum(a, b) {
+    return a + b
+  }
+
+  const imgUrl = "https://thumbs.dreamstime.com/b/ilustração-criativa-abstrata-com-leão-colorido-cor-preta-de-190846410.jpg"
+
+  return (
+    <div className="App">
+      <h2>Alterando o JSX</h2>
+      <br/>
+      <p>Olá, {newName} {newLastName}</p>
+      <br/>
+      <p>A soma de 5 + 5 é: {sum(5,5)}</p>
+      <br/>
+      <img src={imgUrl} width={500} height={300} alt="Minha Imagem" />
+      <HelloWorld />
+    </div>
+  );
+}
+
+export default App;
